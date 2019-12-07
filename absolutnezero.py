f859bccc776092d503dffe37189c7ac728703991
eajaniakcdv <ejaniak2@edu.cdv.pl>
2019-12-07 10:27:51 +0100

    wypychnac pliki chce

---
+++ absolutnezero.py
@@ -0,0 +1,15 @@
+from liczba import Number
+#import liczba - to wtedy jest w kontenerze jakby czyli liczba.number a nie samo number.
+class AbsolutZero(Number):
+    #pass
+    def __init__(self):
+        self.value = 0
+
+    def __str__(self):
+        return("jestes zerem")
+
+    def reklama(self):
+        print("ucz sie!")
+
+zero = AbsolutZero()
+print(zero)
---
+++ liczba.py
@@ -0,0 +1,18 @@
+class Number:
+    # def __new__(self):
+    #     print("Metoda new")
+    def __init__(self,wartosc):
+        print("Metoda init wywolana")
+        self.value = wartosc
+
+    def wyzeruj(self):
+        self.value = 0
+
+    def ustaw(self, ustaw):
+        self.value = ustaw
+
+numer = Number(2)
+#numer2 = Number()
+numer.wyzeruj()
+print(numer.value)
+print (numer.value)
